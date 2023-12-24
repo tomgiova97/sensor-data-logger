65a5af8738fe34d87cf5de8d6f59361442e27065
tommaso-giovannelli-97 <tommaso.giovannelli@bitrock.it>
2023-12-24 11:50:37 +0100

    - Restored default Spring Boot log level - Added logging in ScheduleService to monitor log creation

--- src/main/java/com/example/sensordatalogger/service/LogService.java
+++ src/main/java/com/example/sensordatalogger/service/LogService.java
@@ -1,6 +1,8 @@
 package com.example.sensordatalogger.service;
 
 import com.example.sensordatalogger.model.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.sensordatalogger.repository.LogRepository;
@@ -9,13 +11,17 @@ import java.util.Date;
 
 @Service
 public class LogService {
+    private static final Logger logger = LoggerFactory.getLogger(LogService.class);
     @Autowired
     LogRepository logRepository;
 
     public Log createLog(Log log) {
+        logger.info("Creating log entry...");
         Date currentdate = new Date();
         Date currentDateConvertedToRomeTimezone = new Date(currentdate.getTime() + 60*60*1000); //Adding one hour to the UTC time
         log.setDateTime(currentDateConvertedToRomeTimezone);
+
+        logger.info("Log entry successfully created");
 
         return logRepository.save(log);
     }
